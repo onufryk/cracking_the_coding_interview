1f4fe7137165d72dac7e14224f3e2c229aa65c8b
Roman Onufryk <roman.onufryk@gmail.com>
2018-03-25 20:53:17 -0700

    Question 4.3

---
+++ main/src/com/onufryk/crack/four/three/Question_04_03.java
@@ -0,0 +1,87 @@
+package com.onufryk.crack.four.three;
+
+import java.util.ArrayList;
+
+class Node {
+	public Integer value;
+	public Node left;
+	public Node right;
+	
+	public Node(Integer value) {
+		this.value = value;
+		
+	}
+
+	@Override
+	public String toString() {
+		return this.value.toString();
+	}
+}
+
+class Tree {
+	public Node root;
+
+	public Tree(Node root) {
+		this.root = root;
+	}
+	
+	public Tree() {
+	}
+	
+	public void addNumbers(ArrayList<Integer> numbers) {
+		this.root = this.addSubTree(numbers, 0, numbers.size() - 1);
+	}
+	
+	private Node addSubTree(ArrayList<Integer> numbers, Integer start, Integer end) {
+		if (end < start) {
+			return null;
+		}
+		
+		Integer middle = (start + end) / 2;
+		Node newNode = new Node(numbers.get(middle));
+		newNode.left = this.addSubTree(numbers, start, middle - 1);
+		newNode.right = this.addSubTree(numbers, middle + 1, end);
+		
+		return newNode;
+	}
+	
+	public void traverseInOrder() {
+		System.out.println("Tree in-order:");
+		this.traverseInOrder(this.root);
+	}
+
+	private void traverseInOrder(Node node) {
+		if (node == null) {
+			return;
+		}
+		this.traverseInOrder(node.left);
+		System.out.print(node);
+		System.out.print(" ");
+		this.traverseInOrder(node.right);
+	}
+	
+}
+
+public class Question_04_03 {
+	
+
+	public static void main(String[] args) {
+		ArrayList<Integer> numbers = new ArrayList<>();
+		numbers.add(5);
+		numbers.add(7);
+		numbers.add(15);
+		numbers.add(55);
+		numbers.add(56);
+		numbers.add(500);
+		numbers.add(501);
+		numbers.add(502);
+		
+		System.out.println(numbers);
+		
+		Tree tree = new Tree();
+		tree.addNumbers(numbers);
+		tree.traverseInOrder();
+
+	}
+
+}
